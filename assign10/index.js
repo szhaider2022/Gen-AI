799bcfb447dbb6dae78d177326794504c9b2663f
zeeshan haider <hyder.zeeshan110@gmail.com>
2024-04-16 07:45:40 +0500

    initial commit

---
+++ assign10/index.js
@@ -0,0 +1,5 @@
+/*My name is Syed Zeeshan Haider. I am submitting this short program on Github no dated 16-04-2024.
+Storing my favourate number in a variable and print message with this number
+*/
+var a = 5110;
+console.log('My favorate number is', a);
---
+++ assign10/index.ts
@@ -0,0 +1,6 @@
+/*My name is Syed Zeeshan Haider. I am submitting this short program on Github no dated 16-04-2024. 
+Storing my favourate number in a variable and print message with this number
+*/
+let a =5110
+console.log('My favorate number is',a)
+
