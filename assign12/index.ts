6b391f078b119cceb6c35db102e03a4ee6794af8
zeeshan haider <hyder.zeeshan110@gmail.com>
2024-04-17 22:35:50 +0500

    initial commit

---
+++ assign12/index.ts
@@ -0,0 +1,10 @@
+/*
+12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the person’s name.
+*/
+let names=['Ammar','Mutahir','Ali Mehdi','Zaidi']
+for (var a=0; a<names.length; a++) {
+    //let friends_sequence=names[a]
+    //console.log('Good to see you', names[a]) 
+    console.log(`Good to see you ${names[a]}`) 
+}
