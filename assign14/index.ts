bf6db9b1fe4ddcb824dc8e8e2ed3febdc2ba2d06
zeeshan haider <hyder.zeeshan110@gmail.com>
2024-04-19 07:24:21 +0500

    initial commit

---
+++ assign14/index.ts
@@ -0,0 +1,9 @@
+/*
+Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+*/
+let myfriends =["Ali", "Talib","Jehanzeb","Jokhio","Ebad","Ammar"];
+
+for (var i=0;i<myfriends.length;i++) {
+    console.log(`Mr. ${myfriends[i]} is cooridally invited for dinner at my home`)
+};
