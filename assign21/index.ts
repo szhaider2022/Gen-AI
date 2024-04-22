c0d4e5287f52f6e95cbf96004d3d28064e0d1331
zeeshan haider <hyder.zeeshan110@gmail.com>
2024-04-22 08:36:49 +0500

    initial commit

---
+++ assign21/index.ts
@@ -0,0 +1,21 @@
+/*
+They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+*/
+let objContainingItems = {
+    "laptop": {
+        name: 'laptop',
+        price: 8000,
+        description: 'high performance elite book',
+    },
+    "keyboard": {
+        name: "keyboard",
+        price: 200,
+        description: 'Dell keyboard'
+    },
+    "mouse": {
+        name: 'mouse',
+        price: 20,
+        description: 'A4Tec'
+    }
+}
+console.log(objContainingItems);
