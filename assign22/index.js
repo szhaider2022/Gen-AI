a0c3527f91cfcaca3f6bacdc29b28d81328432f5
zeeshan haider <hyder.zeeshan110@gmail.com>
2024-04-24 18:45:13 +0500

    initial commmit

---
+++ assign22/index.js
@@ -0,0 +1,7 @@
+/*
+Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
+to produce an index error. Make sure you correct the error before closing the program.
+*/
+var a = [1, 2, 3, 4, 5];
+console.log(a[10]);
+console.log(a[1]);
