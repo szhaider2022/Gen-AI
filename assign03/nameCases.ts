6ddfd82e36b824ed668ee7ba9dc7c7b0be190e61
zeeshan haider <hyder.zeeshan110@gmail.com>
2024-05-01 14:07:49 +0500

    initial commit

--- assign03/nameCases.ts
+++ assign03/nameCases.ts
@@ -1,15 +1,16 @@
-let name='syed ZEEshan haider'
+let name ='syed ZEEshan haider'
 // // //Lower case
  console.log(name.toLowerCase())
 // // //Upper case
  console.log(name.toUpperCase())
-// //Title Case
 
+ // //Title Case Method 1
  const newName=name.split(" ").map((l) => l[0].toUpperCase() + 
  l.substr(1)).join(" ");
 
  console.log(newName)
 
+ // //Title Case Method 2
  function TitleCase(str) {
     str=str.toLowerCase();
     str=str.split(' ');
